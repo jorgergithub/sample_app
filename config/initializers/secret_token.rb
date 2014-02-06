547d1742a93b1f049660f3b5526f89cd21b5cce0
Administrator <jorge@roccatagliata.com>
2014-02-05 18:58:12 -0700

    adding initialized app

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,19 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-SampleApp::Application.config.secret_key_base = '5d8dc1ea3efeedc0f38d776d38a1f0d2f8c1d8cdf813199d126a72dd9b5ae926d1e06fab2f1424744fe8868600115f5fc8ee23d6aa207b46e6d42b9ca309105a'
+require 'securerandom'
+
+def secure_token
+  token_file = Rails.root.join('.secret')
+  if File.exist?(token_file)
+    # Use the existing token.
+    File.read(token_file).chomp
+  else
+    # Generate a new token and store it in token_file.
+    token = SecureRandom.hex(64)
+    File.write(token_file, token)
+    token
+  end
+end
+
+SampleApp::Application.config.secret_key_base = secure_token
